d7fe5a99e0c736b8acae62b804170421590b6f24
jonahmabry <jonahmabry@gmail.com>
2025-12-05 20:01:09 -0600

    Days 1 and 2

--- aoc25/src/puzzles/day01.py
+++ aoc25/src/puzzles/day01.py
@@ -5,11 +5,49 @@ def solve_part1(data: str):
     input_data = parse_data(data)
     print(input_data)
 
-    return None
+    count = 0
+    position = 50
+
+    for move in input_data:
+        direction = move[0:1]
+        amount = int(move[1:])
+
+        if direction == 'L':
+            position -= amount
+        else:
+            position += amount
+
+        if not(0 < position < 99):
+            position %= 100
+
+        if position == 0:
+            count += 1
+
+    return count
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
     print(input_data)
 
-    return None
+    count = 0
+    position = 50
+
+    for move in input_data:
+        direction = move[0:1]
+        amount = int(move[1:])
+
+        for _ in range(amount):
+            if direction == 'L':
+                position -= 1
+            else:
+                position += 1
+
+            if not(0 < position < 99):
+                position %= 100
+
+            if position == 0:
+                count += 1
+
+
+    return count
--- aoc25/src/puzzles/day02.py
+++ aoc25/src/puzzles/day02.py
@@ -1,15 +1,53 @@
 from ..utils import parse_data
 
+EXAMPLE = ['11-22,95-115,998-1012,1188511880-1188511890,222220-222224,'
+           '1698522-1698528,446443-446449,38593856-38593862,565653-565659,'
+           '824824821-824824827,2121212118-2121212124']
 
 def solve_part1(data: str):
     input_data = parse_data(data)
+    # input_data = EXAMPLE
     print(input_data)
 
-    return None
+    total = 0
+    endpoints = input_data[0].split(',')
+
+    for endpoint in endpoints:
+        low, high = endpoint.split('-')
+        for i in range(int(low), int(high) + 1):
+            i = str(i)
+            first_half = i[0:len(i)//2]
+            second_half = i[len(i) // 2:]
+
+            if first_half == second_half:
+                # print(i)
+                total += int(i)
+
+    return total
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
+    # input_data = EXAMPLE
     print(input_data)
 
-    return None
+    total = 0
+    endpoints = input_data[0].split(',')
+    sequences = [2,3,5,7]
+
+    for endpoint in endpoints:
+        low, high = map(int, endpoint.split('-'))
+        for i in range(low, high + 1):
+            i = str(i)
+            for seq in sequences:
+                if len(i) % seq == 0:
+                    block_size = len(i) // seq
+                    blocks = []
+                    for j in range(seq):
+                        blocks.append(i[j*block_size:(j+1)*block_size])
+
+                    if len(set(blocks)) == 1:
+                        total += int(i)
+                        break
+
+    return total
