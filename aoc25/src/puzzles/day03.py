9be57f49333b5549caf59b2610fb4908fa47c0fa
jonahmabry <jonahmabry@gmail.com>
2025-12-22 11:43:58 -0600

    Day 3 done

--- aoc25/src/puzzles/day03.py
+++ aoc25/src/puzzles/day03.py
@@ -5,11 +5,34 @@ def solve_part1(data: str):
     input_data = parse_data(data)
     print(input_data)
 
-    return None
+    total = 0
+    for bank in input_data:
+        high = 0
+        for i in range(len(bank)):
+            for j in range(i+1, len(bank)):
+                high = max(high, int(bank[i] + bank[j]))
+        total += high
+
+    return total
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
     print(input_data)
 
-    return None
+    total = 0
+    for bank in input_data:
+        high = ""
+        cur_ix = 0
+        for i in range(12):
+            remaining_num = 11-i
+            search_range = bank[cur_ix : (len(bank) - remaining_num)]
+
+            max_digit = max(search_range)
+            found_at = search_range.index(max_digit)
+
+            high += max_digit
+            cur_ix += found_at + 1
+        print(high)
+        total += int(high)
+    return total
